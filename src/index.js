d2c7c848009bc693a07a3d5b6a9a661cad226577
Mikel <mikel362d@gmail.com>
2022-10-09 07:49:01 -0700

    changes functional to class compponent

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
-    return <div> Hi there</div>
-};
+class App extends React.Component {
+    //not required by react. It belongs to JS
+    // One way to initialize state
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
+
+    //define render is a rect requirement to return a JSX
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+        return <div>Latitude:</div>
+    }
+}
 
 ReactDOM.render(
     <App />,
